4c2ce391a6cddb32f04020487150fcd63facbd32
Shraddhasaini <Shraddhasaini99@gmail>
2020-05-28 14:53:03 +0530

    firebase001

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:whereisevery1/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //return either Home or authenticate widget
+    return Home();
+  }
+}
