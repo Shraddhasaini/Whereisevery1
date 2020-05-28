968b228a0d467ae969e20fb7e806579776dc19dd
Shraddhasaini <Shraddhasaini99@gmail>
2020-05-28 15:04:16 +0530

    firebase

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,7 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService{
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   //sign in ano
+  Future signInAnon() async {
+    try{
+
+    } catch(e){
+
+    }
+  }
   //signin with email pass
   //register with email passs
   //signout
