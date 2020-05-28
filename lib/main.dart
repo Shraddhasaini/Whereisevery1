a6d0ad9a5c0d885ef8f14a71b8f920accfe9b78b
Shraddhasaini <Shraddhasaini99@gmail>
2020-05-28 14:13:43 +0530

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,38 @@ class Splash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: FlatButton(
-              onPressed: (){
-                Navigator.of(context)
-                    .push(
-                      MaterialPageRoute(
-                        builder: (context) => Login()
-                      )
-                );
-              },
-              child: Text(
-                'Montserrat Regular',
-            style: TextStyle(
-              fontFamily: 'Montserrat',
-              fontWeight: FontWeight.bold,
-              fontSize: 30.0
+      backgroundColor: Colors.amberAccent[400],
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 683,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/splashtext.png'),
+                ),
+              ),
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    left: 0,
+                    top: 60,
+                    width: 400,
+                    height: 600,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/splash.png'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
+          ],
         ),
-        ),
+      ),
     );
   }
 }
