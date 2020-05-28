1248d3384e826fe3e215887a55d36a2ea7cd7467
Shraddhasaini <Shraddhasaini99@gmail>
2020-05-28 16:49:17 +0530

    Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,34 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:whereisevery1/login.dart';
+import 'dart:async';
 
 void main() => runApp(MaterialApp(
   home: Splash()
 ));
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+        Duration(seconds: 3),
+            (){
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder:(context) => Login(),
+        ),
+      );
+    },
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
