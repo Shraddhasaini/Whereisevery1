fb7110d51bc7e35ed3594e0cc0228c470c2aac49
Shraddhasaini <Shraddhasaini99@gmail>
2020-05-28 14:39:35 +0530

    firebase

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text('home'),
+    );
   }
 }
