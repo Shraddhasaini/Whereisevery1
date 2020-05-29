0bdfd50e64eb48559db8c496e7101ea8eb3a697c
Shraddhasaini <Shraddhasaini99@gmail>
2020-05-29 10:13:15 +0530

    fri006

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,10 +1,64 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      Duration(seconds: 3),
+          (){
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder:(context) => HomePage(),
+          ),
+        );
+      },
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('home'),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+         child: Column(
+             children: <Widget>[
+           Center(
+             child: TweenAnimationBuilder(
+                    duration: Duration(seconds: 2),
+                   tween: Tween<double>(begin: 0,end: 1),
+                  builder: (_, double size, __) {
+                   return Transform.scale(scale: size,
+                    child: Container(
+                      height: 683,
+                       decoration: BoxDecoration(
+                       image: DecorationImage(
+                         image: AssetImage('assets/cats.png'),
+                   ),
+                  ),
+                ),
+               );
+              },
+             ),
+           ),
+         ],
+        ),
+      ),
     );
   }
 }
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Home'),
+    );
+  }
+}
+
