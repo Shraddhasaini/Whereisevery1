c012d6bf4a6ac79a1333b05f3653ffd4a615c64c
Shraddhasaini <Shraddhasaini99@gmail>
2020-05-30 18:07:29 +0530

    sat006

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -4,3 +4,12 @@ class User {
   User({ this.uid });
 
 }
+
+class UserData {
+  final String uid;
+  final String name;
+  final String location;
+  final String status;
+
+  UserData({ this.uid,this.name,this.location,this.status});
+}
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:whereisevery1/models/status.dart';
+import 'package:whereisevery1/models/users.dart';
 
 class DatabaseService {
 
@@ -28,9 +29,26 @@ class DatabaseService {
     }).toList();
   }
 
+  //userData from snapshot
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot){
+    return UserData(
+      uid: uid,
+      name: snapshot.data['name'],
+      location: snapshot.data['location'],
+      status: snapshot.data['status']
+    );
+  }
+
   // get statuses stream
   Stream<List<Status>> get statuses {
     return statusCollection.snapshots()
     .map(_statusListFromSnapshot);
   }
+
+  //get user doc stream
+ Stream<UserData> get userData {
+    return statusCollection.document(uid).snapshots()
+    .map(_userDataFromSnapshot);
+ }
+
 }
